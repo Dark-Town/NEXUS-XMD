c915b0708a4d05a8f99c9ca0b2d63701b3a6fb85
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-05-20 15:21:24 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "nexus~6ARDwZCB#kCreaQRc9RHEuWJu7S1Bp-EWLpwfFIQKYrWv3xt1pno",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
